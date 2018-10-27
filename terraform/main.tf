6e5ac202fcb7ca94354332885aaf09b269360f12
Taras Zaviryukha <taraszav@gmail.com>
2018-10-27 16:55:45 +0300

    Setup multiple instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,12 @@ provider "google" {
 # Res instance
 
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
+#  name         = "reddit-app"
+  name         = "reddit-app-${count.index}"
   machine_type = "g1-small"
   zone         = "${var.vmzone}"
+  count = "2"
+  
 
   # определение загрузочного диска
   boot_disk {
@@ -26,8 +29,14 @@ resource "google_compute_instance" "app" {
     network = "default"
 
     # использовать ephemeral IP для доступа из Интернет
-    access_config {}
-  }
+    access_config {
+#      nat_ip = "${element(google_compute_address.*.self_link, count.index)}"
+#      nat_ip = "${google_compute_address.default.address}"
+ #    nat_ip = "${lookup(var.instance_ips, count.index)}"
+ # nat_ip = "${google_compute_instance.app.*.network_interface.0.access_config.0.assigned_nat_ip}"
+    }
+  
+ }
 
   #metadata {
   #  ssh-keys = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
@@ -55,11 +64,6 @@ resource "google_compute_instance" "app" {
 resource "google_compute_project_metadata" "ssh_keys" {
    metadata {
      ssh-keys = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
-#    ssh-keys = <<EOF
-#    appuser:${file(var.public_key_path)}
-#    appuser1:${file(var.public_key_path)}
-#    appuser2:${file(var.public_key_path)}
-#    EOF
   }
 }
 
@@ -83,3 +87,15 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
+
+#variable "instance_ips" {
+#  default = {
+#    "0" = "35.241.163.65"
+#    "1" = "35.241.163.66"
+#  }
+#}
+
+#output "externalip" {
+# value = "${google_compute_instance.app.*.network_interface.0.access_config.0.assigned_nat_ip}"
+#}
