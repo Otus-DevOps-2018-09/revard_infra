66aee31949c03ae4cae056b8fb08a38a0006d953
Taras Zaviryukha <taraszav@gmail.com>
2018-10-30 14:21:49 +0000

    Added VPC module

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,23 +1,14 @@
-resource "google_compute_instance" "db" {
-  name         = "reddit-db"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  tags         = ["reddit-db"]
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
 
-  boot_disk {
-    initialize_params {
-      image = "${var.db_disk_image}"
-    }
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
   }
 
-  network_interface {
-    network       = "default"
-    access_config = {}
-  }
-
-  metadata {
-    ssh-keys = "appuser:${file(var.public_key_path)}"
-  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
 }
 
 resource "google_compute_firewall" "firewall_mongo" {
@@ -35,3 +26,4 @@ resource "google_compute_firewall" "firewall_mongo" {
   # порт будет доступен только для инстансов с тегом ...
   source_tags = ["reddit-app"]
 }
+
